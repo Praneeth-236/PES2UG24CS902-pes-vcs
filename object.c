c3b625521175f919238f2d34b7b38fc569b35f75
Praneeth-236 <shettypraneeth840@gmail.com>
2026-04-21 09:13:26 +0000

    Phase 1: Add object_write header computation and hashing

--- object.c
+++ object.c
@@ -94,8 +94,20 @@ int object_exists(const ObjectID *id) {
 //
 // Returns 0 on success, -1 on error.
 int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out) {
-    // TODO: Implement
-    (void)type; (void)data; (void)len; (void)id_out;
+    const char *type_str = type == OBJ_BLOB ? "blob" : (type == OBJ_TREE ? "tree" : "commit");
+    char header[64];
+    int header_len = snprintf(header, sizeof(header), "%s %zu", type_str, len);
+    header_len++; // include null terminator
+
+    size_t full_len = header_len + len;
+    void *full_data = malloc(full_len);
+    if (!full_data) return -1;
+    memcpy(full_data, header, header_len);
+    memcpy((char*)full_data + header_len, data, len);
+
+    compute_hash(full_data, full_len, id_out);
+
+    free(full_data);
     return -1;
 }
 
